9f223e903f784e1151d5125ae2511792a6984fe4
Sebastian G <gayle054@gmail.com>
2021-06-09 07:43:33 -0700

    navbar still there

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-none">
             <div className="container">
-                <Link className="navbar-brand" to="/">MERN Auth</Link>
+                <Link className="navbar-brand " to="/">MERN Auth</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="#navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -28,9 +28,9 @@ const Navbar = (props) => {
                                 <span onClick={props.handleLogout} className="nav-link logout-link">Logout</span>
                             </li>
                         </ul>
-                        : <ul className="navbar-nav ml-auto">
+                        : <ul className="navbar-nav ml-auto"> 
                             <li className="nav-item">
-                                <NavLink className="nav-link"  to="/signup">Create Account</NavLink>
+                                <NavLink className="nav-link"  to="/signup">Create Account</NavLink> 
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link"  to="/login">Login</NavLink>

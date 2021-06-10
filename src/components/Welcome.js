3e4c3f0b054d3c7f7fee24e45fa746ae64ae8699
aprin418 <aprin418@gmail.com>
2021-06-10 10:42:56 -0400

    added journal route

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,40 +1,29 @@
-import React from 'react';
-import birdvid from './videos/birdvid.mp4'
+import React from "react";
+import birdvid from "./videos/birdvid.mp4";
 const Welcome = (props) => {
-    return (
-        <div>
-<<<<<<< HEAD
-          
-            <section>
-            <form className="bird_search_form">
-                <div>
-                    {/* search database on home page */}
-                <label className="HomeSearch">Name</label>  
-                <input className="birdSearch"></input>
-
-                <label className="HomeSearch">Region</label>  
-                <input className="birdSearch"></input>
-
-                <label className="HomeSearch">State</label>  
-                <input className="birdSearch"></input>
-
-                <button className="btn btn-primary specialButton">Submit</button>
-                
-                </div>
-            </form>
-
-=======
-          <section>
-            
->>>>>>> 376f87d79ce6a1ad4767db11eebce7dc52fa0ae8
-            <video autoPlay loop muted  className="video1">
-             
-                <source src={birdvid} type="video/mp4"/>
-            </video>
-            </section>
-        
-        </div>
-    )
-}
+  return (
+    <div>
+      <section>
+        <form className="bird_search_form">
+          <div>
+            {/* search database on home page */}
+            <label className="HomeSearch">Name</label>
+            <input className="birdSearch"></input>
+            <label className="HomeSearch">Region</label>
+            <input className="birdSearch"></input>
+            <label className="HomeSearch">State</label>
+            <input className="birdSearch"></input>
+            <button className="btn btn-primary specialButton">Submit</button>
+          </div>
+        </form>
+      </section>
+      <section>
+        <video autoPlay loop muted className="video1">
+          <source src={birdvid} type="video/mp4" />
+        </video>
+      </section>
+    </div>
+  );
+};
 
 export default Welcome;
